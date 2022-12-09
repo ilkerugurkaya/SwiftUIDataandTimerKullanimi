52df8c604865f6e5a4371e97da37ed254954049e
ilkerugurkaya <kayailkerugur@gmail.com>
2022-12-09 13:17:00 +0300

    Initial Commit

---
+++ DateandTimerKullanimi/DateandTimerKullanimiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateandTimerKullanimiApp.swift
+//  DateandTimerKullanimi
+//
+//  Created by İlker Kaya on 9.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DateandTimerKullanimiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
