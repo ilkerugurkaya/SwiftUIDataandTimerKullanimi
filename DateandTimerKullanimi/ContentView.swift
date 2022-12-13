91b7a95b60e210f42664f234ed1bed9609bab664
ilkerugurkaya <kayailkerugur@gmail.com>
2022-12-13 09:45:50 +0300

    dataandtimer

--- DateandTimerKullanimi/ContentView.swift
+++ DateandTimerKullanimi/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var zaman = Date()
+    @State private var tarihGorun = false
+    @State private var saatGorun = false
+    @State private var tarih = ""
+    @State private var saat = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            TextField("Tarih", text: $tarih)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .onTapGesture {
+                    self.tarihGorun = true
+                }
+            if tarihGorun{
+                DatePicker("",selection: $zaman,displayedComponents: .date).labelsHidden()
+                
+                HStack{
+                    Button {
+                        let zamanFormat = DateFormatter()
+                        zamanFormat.dateFormat = "MM/dd/yyyy"
+                        let alinanTarih = zamanFormat.string(from: self.zaman)
+                        self.tarih = alinanTarih
+                        self.tarihGorun = false
+                    } label: {
+                        Text("Tarih Seç")
+                    }
+                    
+                    Button {
+                        self.tarihGorun = false
+                    } label: {
+                        Text("Kapat").foregroundColor(Color.red)
+                    }
+
+
+                }
+            }
+            
+            TextField("Saat", text: $saat)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .onTapGesture{
+                    self.saatGorun = true
+                }
+            if saatGorun{
+                DatePicker("",selection: $zaman,displayedComponents: .hourAndMinute).labelsHidden()
+                HStack{
+                    Button {
+                        let zamanFormat = DateFormatter()
+                        zamanFormat.dateFormat = "hh:mm"
+                        let alinanSaat = zamanFormat.string(from: self.zaman)
+                        self.saat = alinanSaat
+                        self.saatGorun = false
+                    } label: {
+                        Text("Saat seç")
+                    }
+                    Button {
+                        self.saatGorun = false
+                    } label: {
+                        Text("Kapat")
+                            .foregroundColor(Color.red)
+                    }
+
+                    
+
+                }
+            }
+            
         }
-        .padding()
     }
 }
 
